49604d98a2e6b87ccd810027e71a21daf511b682
Aydiv <55044178+Aydivv@users.noreply.github.com>
2022-03-13 01:26:12 +0000

    Schemas Created

--- src/models.py
+++ src/models.py
@@ -5,10 +5,10 @@ import datetime as _dt
 
 class User(_db.Base):
     __tablename__ = "users"
-    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    id = _sql.Column(_sql.Integer, primary_key=True, index=True, autoincrement=False)
     email = _sql.Column(_sql.String(255), unique=True, index=True)
-    fName = _sql.Column(_sql.String(255))
-    lName = _sql.Column(_sql.String(255))
+    name = _sql.Column(_sql.String(255))
+    priority = _sql.Column(_sql.Integer)
 
     reviews = _orm.relationship("Review", back_populates="reviewer")
 
@@ -19,7 +19,7 @@ class Review(_db.Base):
     user_id = _sql.Column(_sql.Integer, _sql.ForeignKey("users.id"))
     business_id = _sql.Column(_sql.Integer, _sql.ForeignKey("business.id"))
     date_created = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
-    reoly_of = _sql.Column(_sql.Integer, default=_sql.null)
+    reply_of = _sql.Column(_sql.Integer, default=_sql.null)
     
     reviewer = _orm.relationship("User", back_populates="reviews")
     of = _orm.relationship("Business", back_populates="reviews")
@@ -32,22 +32,24 @@ class Business():
     postcode = _sql.Column(_sql.String(255))
     number = _sql.Column(_sql.String(255))
     email = _sql.Column(_sql.String(255))
+    desc = _sql.Column(_sql.String(255))
     website = _sql.Column(_sql.String(255))
     cuisine = _sql.Column(_sql.String(255))
+    scored = _sql.Column(_sql.Boolean)
     
     score = _orm.relationship("Score", back_populates="business")
     reviews = _orm.relationship("Review", back_populates="of")
 
 class Score():
     __tablename__ = "score"
-    id = _sql.Column(_sql.Integer, primary_key=True, index = True)
-    business_id = _sql.Column(_sql.Integer, _sql.ForeignKey("business.id"), primary_key=True)
+    business_id = _sql.Column(_sql.Integer, _sql.ForeignKey("business.id"), primary_key=True, index=True)
     score = _sql.Column(_sql.Integer)
-    tier = _sql.Column(_sql.String(20))
     vegan = _sql.Column(_sql.Boolean)
     singleUsePlastic = _sql.Column(_sql.Boolean)
     foodwasteCollection = _sql.Column(_sql.Boolean)
     localProduce = _sql.Column(_sql.Boolean)
+    latest = _sql.Column(_sql.Boolean)
+    dateOfScore = _sql.Column(_sql.DateTime, default = _dt.datetime.utcnow, primary_key=True,index=True)
 
     business = _orm.relationship("Business", back_populates="score")
 
---
+++ src/schemas.py
@@ -0,0 +1,80 @@
+from typing import List
+import pydantic as _pydantic
+import datetime as _dt
+
+class ReviewBase(_pydantic.BaseModel):
+    user_id: int
+    business_id: int
+    content: str
+
+class createReview(ReviewBase):
+    pass
+
+class Review(ReviewBase):
+    user_name: str
+    date_created: _dt.datetime
+    class Config:
+        orm_mode = True
+
+class UserBase(_pydantic.BaseModel):
+    name: str
+    email: str
+    priority: int
+
+class UserCreate(UserBase):
+    id: int
+
+class User(UserBase):
+    class Config:
+        orm_mode = True
+    pass
+
+class UserReviews(UserBase):
+    reviews: List[Review] = []
+    class Config:
+        orm_mode = True
+    pass
+
+class ScoreBase(_pydantic.BaseModel):
+    score: int
+    business_id: int
+    vegan: bool
+    singleUsePlastic: bool
+    foodwasteCollection: bool
+    localProduce: bool
+
+class ScoreCreate(ScoreBase):
+    latest = True
+
+class Score(ScoreBase):
+    dateOfScore: _dt.datetime
+    class Config:
+        orm_mode = True
+
+class BusinessBase(_pydantic.BaseModel):
+    name: str
+    address: str
+    postcode: str
+    desc: str
+    cuisine: str
+    scored: bool
+
+class BusinessCreate(BusinessBase):
+    number: str
+    email: str
+    website: str
+    pass
+
+class Business(BusinessBase):
+    score: int
+    class Config:
+        orm_mode = True
+
+class BusinessProfile(BusinessBase):
+    scores: List[Score] = []
+    number: str
+    email: str
+    website: str
+    reviews: List[Review] = []
+    class Config:
+        orm_mode = True
