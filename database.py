1519185a42a8ffc5845fbf046417ea5fbbffe0e6
Aydiv <55044178+Aydivv@users.noreply.github.com>
2022-03-29 23:05:09 +0100

    Added pool recycling

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.exc import SQLAlchemyError
 
 SQL_ALCHEMY_DATABASE_URL = "mysql+pymysql://b14a34d3a49e5b:aa7154b7@us-cdbr-east-05.cleardb.net/heroku_c46436a436c08a3"
 
-engine = _sql.create_engine(SQL_ALCHEMY_DATABASE_URL)
+engine = _sql.create_engine(SQL_ALCHEMY_DATABASE_URL, pool_recycle=60)
 
 
 
