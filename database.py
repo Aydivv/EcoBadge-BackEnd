4dae54d5606b66ff337966f594aa8568217526ba
Aydiv <55044178+Aydivv@users.noreply.github.com>
2022-03-29 22:55:46 +0100

    Edited database url

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 from sqlalchemy.exc import SQLAlchemyError
 
-SQL_ALCHEMY_DATABASE_URL = "mysql+pymysql://b14a34d3a49e5b:aa7154b7@us-cdbr-east-05.cleardb.net/heroku_c46436a436c08a3?reconnect=true"
+SQL_ALCHEMY_DATABASE_URL = "mysql+pymysql://b14a34d3a49e5b:aa7154b7@us-cdbr-east-05.cleardb.net/heroku_c46436a436c08a3"
 
 engine = _sql.create_engine(SQL_ALCHEMY_DATABASE_URL)
 
